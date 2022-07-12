3fe9e3c6b0cdeda4c77d59fc98301c4d25cb9243
Keyvan Mosharraf <github@keyvanm.com>
2022-07-12 13:25:38 -0700

    better paginate

--- remote_resources/utils/querysets.py
+++ remote_resources/utils/querysets.py
@@ -1,12 +1,31 @@
+from django.core.paginator import Paginator
 from django.db import models
-from django.db.models import Min, Max
 
 
 class PageableQuerySet(models.QuerySet):
-    def paginate(self, limit):
-        min_id = self.aggregate(m=Min('id'))['m']
-        if min_id is None:
-            return self
-        max_id = self.aggregate(m=Max('id'))['m']
-        for i in range(min_id, max_id + 1, limit):
-            yield self.filter(id__gte=i, id__lt=i + limit)
+    """
+
+    """
+
+    def paginate(self, limit, simple=True):
+        """
+
+        :param limit:
+        :param simple:
+        :return:
+        """
+
+        paginated_pks = tuple(
+            page.object_list
+            for page in Paginator(
+                self.values_list('pk', flat=True),
+                limit
+            )
+        )
+
+        qs = self.model.objects.all() if simple else self
+
+        while paginated_pks:
+            pks_page = paginated_pks[0]
+            yield qs.filter(pk__in=pks_page)
+            paginated_pks = paginated_pks[1:]
