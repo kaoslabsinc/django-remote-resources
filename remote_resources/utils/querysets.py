be6626f6f66017932f3cbe363b13aee2838d969f
Keyvan Mosharraf <github@keyvanm.com>
2022-07-19 12:35:37 -0400

    basic_paginate

--- remote_resources/utils/querysets.py
+++ remote_resources/utils/querysets.py
@@ -1,11 +1,20 @@
 from django.core.paginator import Paginator
 from django.db import models
+from django.db.models import Min, Max
 
 
 class PageableQuerySet(models.QuerySet):
     """
 
     """
+
+    def basic_paginate(self, limit):
+        min_id = self.aggregate(m=Min('id'))['m']
+        if min_id is None:
+            return self
+        max_id = self.aggregate(m=Max('id'))['m']
+        for i in range(min_id, max_id + 1, limit):
+            yield self.filter(id__gte=i, id__lt=i + limit)
 
     def paginate(self, limit, simple=True, mutating=False):
         """
