9a6789e87b884d587a08f2ec44701701b3f5875e
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-11-07 23:46:00 -0500

    enable raw item with TimeStamps, put raw as an interface field

--- remote_resources/raw_items/models.py
+++ remote_resources/raw_items/models.py
@@ -1,14 +1,19 @@
 from typing import Any
 
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 from .querysets import RawItemQuerySet
 
 
-class AbstractRawItem(models.Model):
+class AbstractRawItem(
+    TimeStampedModel,
+    models.Model
+):
     class Meta:
         abstract = True
 
+    raw: Any
     source: Any
     processed_item: models.Model | None
 
