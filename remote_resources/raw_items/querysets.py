7dd3cf4964ab96e784ef15d409633e70415b0036
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-11-23 19:55:25 -0500

    _bulk_update_processed_item

--- remote_resources/raw_items/querysets.py
+++ remote_resources/raw_items/querysets.py
@@ -6,14 +6,17 @@ from django.db.models import Case, Value, When
 class RawItemQuerySet(models.QuerySet):
     process_batch_size = 0
 
-    def process(self, *args, **kwargs):
-        processed_raw_items = []
-        count_updated = 0
-
+    def _bulk_update_processed_item(self, processed_raw_items):
         if isinstance(self.model.processed_item, GenericForeignKey):
             bulk_update_fields = ('_pi_content_type', '_pi_object_id')
         else:
             bulk_update_fields = ('processed_item',)
+
+        return self.bulk_update(processed_raw_items, bulk_update_fields)
+
+    def process(self, *args, **kwargs):
+        processed_raw_items = []
+        count_updated = 0
 
         for obj in self.all():
             processed_item = obj.process(*args, **kwargs)
@@ -21,11 +24,11 @@ class RawItemQuerySet(models.QuerySet):
             processed_raw_items.append(obj)
 
             if len(processed_raw_items) == self.process_batch_size:
-                count_updated += self.bulk_update(processed_raw_items, bulk_update_fields)
+                count_updated += self._bulk_update_processed_item(processed_raw_items)
                 processed_raw_items = []
 
         if processed_raw_items:
-            count_updated += self.bulk_update(processed_raw_items, bulk_update_fields)
+            count_updated += self._bulk_update_processed_item(processed_raw_items)
 
         return count_updated
 
