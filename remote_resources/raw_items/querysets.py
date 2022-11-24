448b59168e00d13325febe624b8454ab02352821
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-11-23 19:54:03 -0500

    Bulk update generic fk processed_item

--- remote_resources/raw_items/querysets.py
+++ remote_resources/raw_items/querysets.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.db.models import Case, Value, When
 
@@ -8,17 +9,23 @@ class RawItemQuerySet(models.QuerySet):
     def process(self, *args, **kwargs):
         processed_raw_items = []
         count_updated = 0
+
+        if isinstance(self.model.processed_item, GenericForeignKey):
+            bulk_update_fields = ('_pi_content_type', '_pi_object_id')
+        else:
+            bulk_update_fields = ('processed_item',)
+
         for obj in self.all():
             processed_item = obj.process(*args, **kwargs)
             obj.processed_item = processed_item
             processed_raw_items.append(obj)
 
             if len(processed_raw_items) == self.process_batch_size:
-                count_updated += self.bulk_update(processed_raw_items, ('processed_item',))
+                count_updated += self.bulk_update(processed_raw_items, bulk_update_fields)
                 processed_raw_items = []
 
         if processed_raw_items:
-            count_updated += self.bulk_update(processed_raw_items, ('processed_item',))
+            count_updated += self.bulk_update(processed_raw_items, bulk_update_fields)
 
         return count_updated
 
